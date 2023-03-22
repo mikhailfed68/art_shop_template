19edbb6cd288eec0bb1e2153e7d8da0e528bfdd7
Mikhail Fedorov <mikhailfedorov1939@gmail.com>
2023-03-22 18:37:29 +0300

    Fix configuration

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls import static
+from django.conf.urls.static import static
 
 
 admin.site.site_header = "Администрирование Art-Vostorg.ru"
@@ -31,4 +31,4 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += [
     path('__debug__/', include('debug_toolbar.urls')),
-]
+    ]

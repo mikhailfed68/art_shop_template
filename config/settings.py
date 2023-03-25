e860540acfceef20914142b95b299e96f346c7bd
Mikhail Fedorov <mikhailfedorov1939@gmail.com>
2023-03-25 12:58:31 +0300

    Add url_replacer tag

--- config/settings.py
+++ config/settings.py
@@ -98,6 +98,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            "libraries": {
+                "url_replacer": "templatetags.url_replacer",
+            },
         },
     },
 ]
---
+++ templatetags/url_replacer.py
@@ -0,0 +1,23 @@
+from django import template
+
+register = template.Library()
+
+
+# This tag was created to be used to keep the pagination state
+# with search query, when visitor steps through
+# each of the individual pages
+@register.simple_tag(takes_context=True)
+def replace_param(context, **kwargs):
+    """
+    Replaces the parameters with new values
+    in query string and removes those
+    that have no values.
+    """
+    querydict = context["request"].GET.copy()
+    for param, value in kwargs.items():
+        querydict[param] = value
+
+    for param in [param for param, value in querydict.items() if not value]:
+        del querydict[param]
+
+    return querydict.urlencode()
